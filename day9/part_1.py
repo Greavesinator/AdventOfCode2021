8ddf95df7986797e58c3a175de914bb944932394
jgreaves <jake.greaves@analog.com>
2021-12-13 12:37:26 +0000

    Day9 solution

--- day9/part_1.py
+++ day9/part_1.py
@@ -1,27 +1,61 @@
 
 import time
 
+
 class MapPoint:
     def __init__(self, value) -> None:
         self.value = value
 
-        self.north = False
-        self.east = False
-        self.south = False
-        self.west = False
-        pass
+        self.north = True
+        self.east = True
+        self.south = True
+        self.west = True
+
     def isLowPoint(self):
         return self.north and self.east and self.south and self.west
+
 
 class Map:
     def __init__(self, map_strs) -> None:
         self.map = []
+        self.low_points = []
 
         for i, line in enumerate(map_strs):
             self.map.append([])
             for c in line:
                 if c != '\n':
                     self.map[i].append(MapPoint(int(c)))
+
+    def fetchLowPoints(self):
+        for y in range(len(self.map)):
+            for x, point in enumerate(self.map[y]):
+                # Check point is not already invalidated.
+                if point.isLowPoint():
+                    # Assess points around this.
+                    if y != 0:
+                        point_n = self.map[y - 1][x]
+                        point.north = point.value < point_n.value
+                        point_n.south = point_n.value < point.value
+
+                    if x != (len(self.map[y]) - 1):
+                        point_e = self.map[y][x + 1]
+                        point.east = point.value < point_e.value
+                        point_e.west = point_e.value < point.value
+
+                    if y != (len(self.map) - 1):
+                        point_s = self.map[y + 1][x]
+                        point.south = point.value < point_s.value
+                        point_s.north = point_s.value < point.value
+
+                    if x != 0:
+                        point_w = self.map[y][x - 1]
+                        point.west = point.value < point_w.value
+                        point_w.east = point_w.value < point.value
+
+                if point.isLowPoint():
+                    self.low_points.append(point.value)
+
+        return self.low_points
 
 
 input_data = open("day9/input_data.txt", 'r')
@@ -30,6 +64,15 @@ start = time.perf_counter()
 
 m = Map(input_data.readlines())
 
+lp = m.fetchLowPoints()
+# Get risk (1+height)
+sum = 0
+for p in lp:
+    sum += p+1
+
 end = time.perf_counter()
+
+print(lp)
+print(sum)
 
 print("Time elapsed: ", (end - start)*1000.0, "us")
--- day9/part_2.py
+++ day9/part_2.py
@@ -2,89 +2,126 @@
 import time
 
 
-input_data = open("day8/input_data.txt", 'r')
+class MapPoint:
+    def __init__(self, value) -> None:
+        self.value = value
 
-zero = ''
-one = ''
-two = ''
-three = ''
-four = ''
-five = ''
-six = ''
-seven = ''
-eight = ''
-nine = ''
+        self.north = True
+        self.east = True
+        self.south = True
+        self.west = True
 
-hit_count = 0
+    def isLowPoint(self):
+        return self.north and self.east and self.south and self.west
+
+
+class Map:
+    def __init__(self, map_strs) -> None:
+        self.map = []
+        self.basins = []
+
+        for i, line in enumerate(map_strs):
+            self.map.append([])
+            for c in line:
+                if c != '\n':
+                    self.map[i].append(MapPoint(int(c)))
+
+    def fetchBasins(self):
+        for y in range(len(self.map)):
+            for x, point in enumerate(self.map[y]):
+                # Check point is not already invalidated.
+                if point.isLowPoint():
+                    # Assess points around this.
+                    if y != 0:
+                        point_n = self.map[y - 1][x]
+                        point.north = point.value < point_n.value
+                        point_n.south = point_n.value < point.value
+
+                    if x != (len(self.map[y]) - 1):
+                        point_e = self.map[y][x + 1]
+                        point.east = point.value < point_e.value
+                        point_e.west = point_e.value < point.value
+
+                    if y != (len(self.map) - 1):
+                        point_s = self.map[y + 1][x]
+                        point.south = point.value < point_s.value
+                        point_s.north = point_s.value < point.value
+
+                    if x != 0:
+                        point_w = self.map[y][x - 1]
+                        point.west = point.value < point_w.value
+                        point_w.east = point_w.value < point.value
+
+                if point.isLowPoint():
+                    v = point.value
+
+                    # Assess basins
+                    points = [[x, y]]
+                    size = 0
+                    for p in points:
+                        # Add points if they are greater and not 9 to keep this loop running
+                        p_x = p[0]
+                        p_y = p[1]
+
+                        point = self.map[p_y][p_x]
+
+                        if point.value != 9:
+                            size += 1
+
+                            if p_y != 0:
+                                point_n = self.map[p_y - 1][p_x]
+                                if point.value < point_n.value:
+                                    add_p = [p_x, p_y-1]
+                                    if add_p not in points:
+                                        points.append(add_p)
+
+                            if p_x != (len(self.map[p_y]) - 1):
+                                point_e = self.map[p_y][p_x + 1]
+                                if point.value < point_e.value:
+                                    add_p = [p_x + 1, p_y]
+                                    if add_p not in points:
+                                        points.append(add_p)
+
+                            if p_y != (len(self.map) - 1):
+                                point_s = self.map[p_y + 1][p_x]
+                                if point.value < point_s.value:
+                                    add_p = [p_x, p_y+1]
+                                    if add_p not in points:
+                                        points.append(add_p)
+
+                            if p_x != 0:
+                                point_w = self.map[p_y][p_x - 1]
+                                if point.value < point_w.value:
+                                    add_p = [p_x - 1, p_y]
+                                    if add_p not in points:
+                                        points.append(add_p)
+
+                    # if len(self.basins) != 3:
+                    self.basins.append(size)
+                    # else:
+                    #     for i in range(len(self.basins)):
+                    #         if self.basins[i][1] < size:
+                    #             self.basins[i] = [v, size]
+                    #             break
+
+        return self.basins
+
+
+input_data = open("day9/input_data.txt", 'r')
 
 start = time.perf_counter()
 
-sum = 0
+m = Map(input_data.readlines())
 
-for line in input_data.readlines():
-    number_dict = {}
-    len_5_list = []
-    len_6_list = []
-    # Build our dict of simple digits.
-    for code in line.split(' | ')[0].split():
-        code = "".join(sorted(code))
-        if len(code) == 2:
-            number_dict[code] = '1'
-            one = code
-        elif len(code) == 4:
-            number_dict[code] = '4'
-            four = code
-        elif len(code) == 3:
-            number_dict[code] = '7'
-            seven = code
-        elif len(code) == 7:
-            number_dict[code] = '8'
-            eight = code
-        elif len(code) == 5:
-            # Decide if its 2,3 or 5
-            len_5_list.append(code)
-        elif len(code) == 6:
-            # Decide if its 0,6,9
-            len_6_list.append(code)
+basins = m.fetchBasins()
+basins.sort(reverse=True)
 
-    # Deduce awkward chars.
-    # Essentially difference with found characters can be unique in cases.
-    # Use this to identify the remaining character codes.
-    for code in len_6_list:
-        code = "".join(sorted(code))
-        if len(''.join(set(code).intersection(one))) == 1:
-            number_dict[code] = '6'
-            six = code
-        elif len(''.join(set(code).intersection(four))) == 3:
-            number_dict[code] = '0'
-            zero = code
-        elif len(''.join(set(code).intersection(four))) == 4:
-            number_dict[code] = '9'
-            nine = code
-    for code in len_5_list:
-        code = "".join(sorted(code))
-        if len(''.join(set(code).intersection(four))) == 2:
-            number_dict[code] = '2'
-            two = code
-        else:
-            if len(''.join(set(code).intersection(six))) == 4:
-                number_dict[code] = '3'
-                three = code
-            elif len(''.join(set(code).intersection(six))) == 5:
-                number_dict[code] = '5'
-                five = code
-
-    # Parse output.
-    curr_result = ''
-    four_digit_string = line.split(' | ')[1]
-    for code in four_digit_string.split():
-        code = "".join(sorted(code))
-        curr_result += number_dict[code]
-
-    print(curr_result)
-    sum += int(curr_result)
+product = 1
+for i in range(0, 3):
+    product *= basins[i]
 
 end = time.perf_counter()
+print(basins)
+print(product)
 
-print(sum)
 print("Time elapsed: ", (end - start)*1000.0, "us")
